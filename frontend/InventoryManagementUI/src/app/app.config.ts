b854b12c6233f315951f830f929db70bf50f173f
Kahan2607 <kahan2607@gmail.com>
2025-04-03 09:59:50 +0530

    applied prettier to app.routes and app.config

--- frontend/InventoryManagementUI/src/app/app.config.ts
+++ frontend/InventoryManagementUI/src/app/app.config.ts
@@ -1,20 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http'; 
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),   
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAuth0({
       domain: 'kahan2607.jp.auth0.com',
       clientId: 'fDvyk8ovpSWD9ivJYNmoPtCtqWgJMGzM',
       authorizationParams: {
-        redirect_uri: `${window.location.origin}/home`
-      }
+        redirect_uri: `${window.location.origin}/home`,
+      },
     }),
     provideHttpClient(),
-  ]
+  ],
 };
--- frontend/InventoryManagementUI/src/app/app.routes.ts
+++ frontend/InventoryManagementUI/src/app/app.routes.ts
@@ -6,61 +6,68 @@ import { Auth0Component } from './auth0/auth0.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
 
 export const routes: Routes = [
-    {
-        path: '', 
-        loadComponent: () => {
-            return import('./auth0/auth0.component').then(m => m.Auth0Component); // lazy loading of the auth0 component
-        }
-
+  {
+    path: '',
+    loadComponent: () => {
+      return import('./auth0/auth0.component').then((m) => m.Auth0Component); // lazy loading of the auth0 component
     },
-    {
-        path: 'home', 
-        loadComponent: () => {
-            return import('./home/home.component').then(m => m.HomeComponent); // lazy loading
-        } 
-
+  },
+  {
+    path: 'home',
+    loadComponent: () => {
+      return import('./home/home.component').then((m) => m.HomeComponent); // lazy loading
     },
-    {
-        path: 'categories',
-        loadComponent: () => {
-            return import('./category/category.component').then((m) => m.CategoryComponent);
-        }
+  },
+  {
+    path: 'categories',
+    loadComponent: () => {
+      return import('./category/category.component').then(
+        (m) => m.CategoryComponent
+      );
     },
-    {
-        path: 'items',
-        loadComponent: () => {
-            return import('./item/item.component').then(m => m.ItemComponent);
-        }
+  },
+  {
+    path: 'items',
+    loadComponent: () => {
+      return import('./item/item.component').then((m) => m.ItemComponent);
     },
-    {
-        path: 'items/add-item',
-        loadComponent: () => {
-            return import('./components/add-item/add-item.component').then( m => m.AddItemComponent);
-        }
+  },
+  {
+    path: 'items/add-item',
+    loadComponent: () => {
+      return import('./components/add-item/add-item.component').then(
+        (m) => m.AddItemComponent
+      );
     },
-    {
-        path: 'items/update-item',
-        loadComponent: () => {
-            return import('./components/add-item/add-item.component').then( m => m.AddItemComponent);
-        }
+  },
+  {
+    path: 'items/update-item',
+    loadComponent: () => {
+      return import('./components/add-item/add-item.component').then(
+        (m) => m.AddItemComponent
+      );
     },
-    {
-        path: 'sales',
-        loadComponent: () => {
-            return import('./sale/sale.component').then( m => m.SaleComponent );
-        }
+  },
+  {
+    path: 'sales',
+    loadComponent: () => {
+      return import('./sale/sale.component').then((m) => m.SaleComponent);
     },
-    {
-        path: 'sales/add-sales',
-        loadComponent: () => {
-            return import('./components/add-sale/add-sale.component').then(m => m.AddSaleComponent);
-        }
+  },
+  {
+    path: 'sales/add-sales',
+    loadComponent: () => {
+      return import('./components/add-sale/add-sale.component').then(
+        (m) => m.AddSaleComponent
+      );
     },
-    {
-        path: 'sales/update-sales',
-        loadComponent: () => {
-            return import('./components/add-sale/add-sale.component').then( m => m.AddSaleComponent);
-        }
+  },
+  {
+    path: 'sales/update-sales',
+    loadComponent: () => {
+      return import('./components/add-sale/add-sale.component').then(
+        (m) => m.AddSaleComponent
+      );
     },
-
+  },
 ];
